1bf508d567ce89a0137b6476a1e41910f4899ed7
Omri Baram <omribaram@gmail.com>
2021-06-24 22:44:16 +0300

    fun

--- js/router.js
+++ js/router.js
@@ -1,9 +1,14 @@
 import emailApp from './email/pages/email-app.js';
+import emailCompose from './email/cmps/email-compose.js';
 import keepApp from './keep/pages/keep-app.js';
 
 const routes = [{
         path: '/email/:emailId?',
         component: emailApp,
+    },
+    {
+        path: '/email/composeFromNote/:note?',
+        component: emailCompose,
     },
     {
         path: '/keep',
