6779c6656501e6a0acd238b84c0cc972588a3910
omribaram <84462496+omribaram@users.noreply.github.com>
2021-06-25 02:41:33 +0300

    Update email-search.js

--- js/email/cmps/email-search.js
+++ js/email/cmps/email-search.js
@@ -1,7 +1,7 @@
 export default {
     template: `
         <form @submit.prevent="onSearch" class="form-email-search">
-            <img src="../../img/apps/email/search.svg"><input v-model="searchBy.txt" @search="onSearch" type="search" placeholder="Search..." class="input-email-search">
+            <img src="img/apps/email/search.svg"><input v-model="searchBy.txt" @search="onSearch" type="search" placeholder="Search..." class="input-email-search">
             <span class="tooltip">
                 <input v-model="searchBy.isRead" type="checkbox" id="email-read-state" class="input-email-search">
                 <label class="email-read-state" for="email-read-state"></label>
