eaab2fd8cf8f0630c06a3430ff371af5be0b1e4c
Omri Baram <omribaram@gmail.com>
2021-06-26 20:47:02 +0300

    git path

--- js/cmps/loader.js
+++ js/cmps/loader.js
@@ -1,7 +1,7 @@
 export default {
     template: `
     <div class="loader">
-        <img src="../../img/loader/loader.svg" alt="Loader" />
+        <img src="img/loader/loader.svg" alt="Loader" />
     </div>
     `,
 };
