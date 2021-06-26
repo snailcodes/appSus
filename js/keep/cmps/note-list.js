d61196e5098ad579087326f2ba15725748ab8f84
RV <racheli.vardi@gmail.com>
2021-06-26 20:03:21 +0300

    fixes

--- js/keep/cmps/note-list.js
+++ js/keep/cmps/note-list.js
@@ -18,7 +18,8 @@ export default {
 					<button  title="Gray"  class="button-keep" @click="changeBcg('gray',note)"> <span  class="circle circle-gray"></span> </button>
 					<button  title="Blue"  class="button-keep" @click="changeBcg('lightblue',note)"> <span  class="circle circle-lightblue"></span> </button>
 					<button  title="Green"  class="button-keep" @click="changeBcg('lightgreen',note)"> <span  class="circle circle-lightgreen"></span> </button>
-				
+
+
 					<img title="Click to Pin" class="keep-button-img" @click="onPin(note)" :src="note.pinSrc" > 
 				
 					<button title="Delete Note" class="button-keep" @click="onDelNote(note)"> <img class="keep-button-img" src="img/apps/keep/delete.png" alt="Delete"> </button>
